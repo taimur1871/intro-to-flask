ec45859c1ce3a091bf108b48a14712be2c0335f3
Rana Ahmad <taimur1871@gmail.com>
2021-01-21 23:00:24 -0700

    update inference.py

--- inference.py
+++ inference.py
@@ -13,8 +13,19 @@ def get_category(img):
     Returns:
         [str]: Prediction
     """
+    # open image
+    interpreter = tf.lite.Interpreter(model_path="converted_model.tflite")
+    interpreter.allocate_tensors()
+
+    # get input and output tensors
+    input_index = interpreter.get_input_details()
+    output_index = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_index, img)
+    interpreter.invoke()
+    predictions = interpreter.get_tensor(output_index)
     
-    pass
+    return predictions
 
 
 def plot_category(img):
@@ -23,4 +34,5 @@ def plot_category(img):
     Args:
         img [jpg]: image file
     """
-    pass
+    fig = plt.figure(figsize=(8,8))
+    plt.imshow(img)
