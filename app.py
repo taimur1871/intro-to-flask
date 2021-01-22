64ec56fc5d0c841b119b5a9d5ae746233b13f5bf
Rana Ahmad <taimur1871@gmail.com>
2021-01-22 11:35:02 -0700

    adding final files and outputs

--- app.py
+++ app.py
@@ -3,34 +3,20 @@ from inference import get_category, plot_category
 
 app = Flask(__name__)
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
 @app.route('/', methods=['GET', 'POST'])
 def rock_paper_scissor():
-    # Write the GET Method to get the index file
     if request.method == 'POST':
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect('index.html')
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-        render_template('index.html')
-        
-    # Write the POST Method to post the results file
-
-
-    # Read file from upload
-    # Get category of prediction
-    # Plot the category
-    # Render the result template
-    
-    return render_template('result.html')
-
+    # POST Method to post the results file
+        # Read file from upload
+        img = request.files['file']
+        # Get category of prediction
+        image_category = get_category(img)
+        # Plot the category
+        plot_category(img)
+        # Render the result template
+        return render_template('result.html', category=image_category)
+    # For GET requests, load the index file
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
